5c83a61be0548363474d2fa4ee2a5cf060960244
spaceofmiah <geek_200@outlook.com>
2020-04-12 17:24:14 +0100

    Create a skeletal mockup for crime app

---
+++ crimemap.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+from dbhelper import DBHelper
+
+
+app = Flask(__name__)
+DB = DBHelper()
+
+
+
+@app.route('/')
+def home():
+	try:
+		data = DB.get_all_inputs()
+	except Exception as err:
+		print(err.message)
+	return render_template('index.html', data=data)
+
+
+@app.route('add', methods=['POST'])
+def add():
+	try:
+		data = request.form.get('userinput')
+		DB.add_input(data)
+	except Exception as err:
+		print(err.message)
+	return home()
+
+
+@app.route('/clear')
+def clear():
+	try:
+		DB.clear_all()
+	except Exception as err:
+		print(err)
+	return home()
+
+
+if __name__ = '__main__':
+	app.run(port=5000, debug=True)
---
+++ dbhelper.py
@@ -0,0 +1,52 @@
+import pymysql
+import dbconfig
+
+
+class DBHelper:
+
+	def connect(self, database='crimemap'):
+		return pymysql.connect(host='localhost', 
+			user=dbconfig.DB_USERNAME,
+			password=dbconfig.DB_PASSWORD,
+			db=database)
+
+
+	def get_all_inputs(self):
+		connection = self.connect()
+		try:
+			query = "SELECT description from crimes;"
+			with connection.cursor() as cursor:
+				cursor.execute(query)
+			return cursor.fetchall()
+		except Exception as e:
+			print(e)
+		finally:
+			connection.close()
+
+
+	def add_input(self, data):
+		connection = self.connect()
+		try:
+			# The following introduces a deliberate security flaw
+			# see section on SQL injection below
+			query = f"INSERT INTO crimes (description) VALUES ('{data}');"
+			with connection.cursor() as cursor:
+				cursor.execute(query)
+				connection.commit()
+		except Exception as e:
+			print(e)
+		finally:
+			connection.close()
+
+
+	def clear_all(self):
+		connection = self.connect()
+		try:
+			query = "DELETE FROM crimes;"
+			with connection.cursor() as cursor:
+				cursor.execute(query)
+				connection.commit()
+		except Exception as e:
+			print(e)
+		else:
+			connection.close()
